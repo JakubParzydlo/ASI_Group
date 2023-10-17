69f8f35ddcc8123c5af168c6f9dc32eb95c4a9b3
Katarzyna Wartanowicz <s22074@pjwstk.edu.pl>
2023-10-17 18:51:28 +0200

    Split train and evaluate linear discriminant model into two separate functions

--- asi-proj-2/models/linear_discriminant.py
+++ asi-proj-2/models/linear_discriminant.py
@@ -3,13 +3,22 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
 
-def exec(df):
+def train_model(df):
     values = df.values
     X = values[:, 0:9]
     y = values[:, 9]
 
     model = LinearDiscriminantAnalysis()
     cv = KFold(n_splits=3, shuffle=True, random_state=1)
+
+    return cv
+
+def evaluate_model(df, cv):
+    values = df.values
+    X = values[:, 0:9]
+    y = values[:, 9]
+    
+    model = LinearDiscriminantAnalysis()
 
     result = cross_val_score(model, X, y, cv=cv, scoring='accuracy')
 
