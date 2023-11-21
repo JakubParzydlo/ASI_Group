10931043c1b32f80df177383d3b14fc7d3755229
s22074 <101325477+s22074@users.noreply.github.com>
2023-11-21 20:31:13 +0100

    added psql config + changed train_model
    Changes to train_model:
    - instantiated TabularPredictor
    - move label and eval_metric from fit to TabularPredictor()
    - removed visualizer because our version of autogluon doesn't have it as a fti() parameter

--- asi-kedro/src/asi_kedro/pipelines/data_science/nodes.py
+++ asi-kedro/src/asi_kedro/pipelines/data_science/nodes.py
@@ -9,21 +9,20 @@ from sklearn.metrics import accuracy_score, mean_absolute_error, r2_score
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from autogluon.tabular import TabularPredictor as task
+from autogluon.tabular import TabularPredictor
 from autogluon.tabular import TabularDataset
 
 def train_model(df: TabularDataset) -> LinearRegression:
     # Assuming the last column of df is 'target' and rest are features
-    predictor = task.fit(train_data=df, 
-                     label='Potability',  
-                     eval_metric='log_loss', 
+    label_column = 'Potability'
+    task = TabularPredictor(label=label_column, eval_metric='log_loss')
+    predictor = task.fit(train_data=df,   
                      auto_stack=True,
-                     verbosity=2,
-                     visualizer='tensorboard')
+                     verbosity=2)
 
     return predictor
 
-def test_model(predictor: task, test_data: pd.DataFrame) -> DataFrame:
+def test_model(predictor: TabularPredictor, test_data: pd.DataFrame) -> DataFrame:
     
     # Return metrics in a dictionary
     return pd.DataFrame(predictor.predict_proba(test_data, as_pandas=True))
